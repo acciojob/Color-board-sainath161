48faf9fd34f62b7729f331a458fb39b74870d002
Sainath <71361447+sainath161@users.noreply.github.com>
2023-11-02 20:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const container = document.getElementById('container');
+
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Create 800 squares with different hover colors
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+        const randomColor = getRandomColor();
+        square.style.backgroundColor = randomColor; // Change to a random background color on hover
+    });
+
+    square.addEventListener('mouseout', () => {
+        // Reset the background color after 1 second
+        setTimeout(() => {
+            square.style.backgroundColor = ''; // Reset to the original color (from CSS)
+        }, 1000); // 1000 milliseconds = 1 second
+    });
+
+    container.appendChild(square);
+}
